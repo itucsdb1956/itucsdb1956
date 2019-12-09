04e59ab1c9e93a387577e33a3cb192df10520c61
Cihat Bostancı <40023574+CihatBostanci@users.noreply.github.com>
2019-12-10 00:34:59 +0300

    Update dbinit.py

--- dbinit.py
+++ dbinit.py
@@ -30,8 +30,7 @@ if __name__ == "__main__":
         print("Usage: DATABASE_URL=url python dbinit.py", file= sys.stderr)
         sys.exit(1)
     initialize(url)
-    execute(url, showDB)
-
+   
 
 
 
